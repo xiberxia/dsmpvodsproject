1f25b9877d5cf8e40e6cd9875a9a16fcf5d8041a
xiberxia <tyhjdragon@gmail.com>
2025-09-10 01:32:20 -0400

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/navbar";
 import "./globals.css";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
